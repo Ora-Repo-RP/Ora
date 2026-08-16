0f3def7e19e92d7b54c25e910562162326f2f9da
Ora-Repo-RP <ramrp1460@gmail.com>
2026-08-16 21:26:30 +0530

    Update progress bar symbols in _play.py

--- Jani_Music/helpers/kb/_play.py
+++ Jani_Music/helpers/kb/_play.py
@@ -34,25 +34,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100 if duration_sec else 0
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "█░░░░░░░░░░░"
+        bar = "☉━━━━━━━━━━━━"
     elif 10 < umm < 20:
-        bar = "██░░░░░░░░░░"
+        bar = "━☉━━━━━━━━━━━"
     elif 20 <= umm < 30:
-        bar = "███░░░░░░░░░"
+        bar = "━━☉━━━━━━━━━━"
     elif 30 <= umm < 40:
-        bar = "████░░░░░░░░"
+        bar = "━━━☉━━━━━━━━━"
     elif 40 <= umm < 50:
-        bar = "█████░░░░░░░"
+        bar = "━━━━☉━━━━━━━━"
     elif 50 <= umm < 60:
-        bar = "██████░░░░░░"
+        bar = "━━━━━━☉━━━━━━" 
     elif 60 <= umm < 70:
-        bar = "███████░░░░░"
+        bar = "━━━━━━━☉━━━━━"
     elif 70 <= umm < 80:
-        bar = "████████░░░░"
+        bar = "━━━━━━━━☉━━━━"
     elif 80 <= umm < 95:
-        bar = "█████████░░░"
+        bar = "━━━━━━━━━━☉━━"
     else:
-        bar = "████████████"
+        bar = "━━━━˹𝗢꧊᱂𝛂˼━━━━━"
     buttons = [
          [
             primary_button(
@@ -73,7 +73,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 #callback_data="GetTimer",
            # )
        # ],
-        #[danger_button(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [danger_button(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -86,7 +86,7 @@ def stream_markup(_, chat_id):
             success_button(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             success_button(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-       # [danger_button(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [danger_button(text=_["CLOSE_BUTTON"], callback_data="close")],
      ]
     return buttons
 
